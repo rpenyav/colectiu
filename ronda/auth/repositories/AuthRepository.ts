4b039f9b46e6d20bc73b4776bacdcc9a7144f8ee
Rafa <rafa@wad.cat>
2025-09-06 18:47:16 +0200

    "deleteall"

--- ronda/auth/repositories/AuthRepository.ts
+++
@@ -1,3 +0,0 @@
-export interface AuthRepository {
-  login(username: string, password: string): Promise<string>;
-}
--- ronda/i18n/useTranslation.ts
+++
@@ -1,9 +0,0 @@
-import i18n from "./i18n";
-
-const useTranslation = () => {
-  return {
-    t: i18n.t.bind(i18n),
-  };
-};
-
-export default useTranslation;
